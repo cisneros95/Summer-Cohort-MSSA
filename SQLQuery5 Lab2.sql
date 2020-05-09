2fc031fd7e27f601e78c1df8ce34b7be739c7b9c
Franco <62530407+cisneros95@users.noreply.github.com>
2020-05-08 20:12:32 -0700

    Delete SQLQuery5 Lab2.sql

--- SQLQuery5 Lab2.sql
+++
@@ -1,9 +0,0 @@
---SELECT *
---From SalesLT
-
-
---SELECT Title, FirstName, MiddleName, LastName, Suffix
---FROM SalesLT.Customer;
-
---SELECT SalesPerson, ISNULL(Title, '') + ISNULL(FirstName, '') + ISNULL(MiddleName, '') + ISNULL(LastName, '') + ISNULL(Suffix, '') AS CustomerName, Phone
---From SalesLT.Customer
